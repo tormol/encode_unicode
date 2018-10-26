ca2e5fc3393d440e6c933e3bdb094680241ddc93
tormol <t.b.moltu@lyse.net>
2018-10-26 18:18:11 +0200

    Remove re-exports of Utf8Iterator and Utf16Iterator from the crate root
    They're still accessible through the iterator module.

--- src/lib.rs
+++ src/lib.rs
@@ -68,8 +68,6 @@ mod decoding_iterators;
 pub use traits::{CharExt, U8UtfExt, U16UtfExt, StrExt, IterExt, SliceExt};
 pub use utf8_char::Utf8Char;
 pub use utf16_char::Utf16Char;
-pub use utf8_iterators::Utf8Iterator;
-pub use utf16_iterators::Utf16Iterator;
 
 pub mod error {// keeping the public interface in one file
     //! Errors returned by various conversion methods in this crate.
