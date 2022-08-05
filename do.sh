1e38235c481d7ea9abaa88ce86e4567c54af4264
tormol <t.b.moltu@lyse.net>
2022-08-05 19:22:14 +0200

    Lock dependencies to the specified minor version when building at MSRV

--- do.sh
+++ do.sh
@@ -6,7 +6,7 @@ FUZZ_DURATION=60
 FUZZ_PAUSE=2
 
 if [[ ${1:0:1} == - || $1 == help ]] || (( $# > 1 )); then
-    echo "Usage: $0 ([setup|[check|MSRV]|test|ignored|clippy|fuzz|test|help])" >&2
+    echo "Usage: $0 ([setup|MSRV|check|test|ignored|clippy|fuzz|test|help])" >&2
     echo "If no argument is provided, all parts except ignored and help are run," >&2
     echo "but setup is only done if auto-detection fails." >&2
     exit 1
@@ -16,23 +16,29 @@ fi
 if [[ $1 == setup ]] || ! rustup show | grep --silent "$MSRV"; then
     rustup install "$MSRV" --no-self-update
 fi
-if [[ -z $1 || $1 == msrv || $1 == MSRV || $1 == check ]]; then
-    cargo "+$MSRV" check --tests --examples --no-default-features
-    cargo "+$MSRV" check --tests --examples
-    cargo "+$MSRV" build --tests --examples --all-features
+if [[ -z $1 || $1 == msrv ]]; then
+    # FIXME modify Cargo.toml like on CI, and then restore it and Cargo.lock afterwards
+    cargo "+$MSRV" build --all-features
 fi
 
-# stable
+# check all feature combinations
+if [[ -z $1 || $1 == check ]]; then
+    cargo check --examples --tests --no-default-features
+    cargo check --examples --tests
+    cargo check --examples --tests --all-features
+fi
+
+# tests
 if [[ -z $1 || $1 == test ]]; then
     cargo test --all-features -- --quiet
 elif [[ $1 == ignored ]]; then
     cargo test --all-features -- --quiet --ignored
 fi
 
+# clippy, nightly
 if [[ $1 == setup ]] || ! rustup show | grep --silent nightly; then
     rustup install nightly --no-self-update
 fi
-# clippy
 if [[ $1 == setup ]] || ! cargo +nightly help clippy >/dev/null 2>/dev/null; then
     rustup component add clippy --toolchain nightly
 fi
@@ -40,7 +46,7 @@ if [[ -z $1 || $1 == clippy ]]; then
     cargo +nightly clippy --all-features --tests --benches --examples
 fi
 
-# fuzzing tests
+# fuzzing tests, nightly
 if [[ $1 == setup ]] || ! command -V cargo-fuzz >/dev/null 2>/dev/null; then
     cargo +nightly install cargo-fuzz
 fi
@@ -56,7 +62,7 @@ if [[ -z $1 || $1 == fuzz ]]; then
     done
 fi
 
-# benchmarks
+# benchmarks, nightly
 if [[ -z $1 || $1 == bench ]]; then
     cargo +nightly check --benches --no-default-features
     cargo +nightly check --benches
